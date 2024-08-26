d1241da32c4106e5591204f275284ae90d5d7217
SyedMuhammadKhalid <khalidshah98765@gmail.com>
2024-08-26 11:09:35 +0000

    udpdated

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask, render_template
 app= Flask(__name__)
+JOBS =[
+    {
+        'id':1,
+        'title':'Team Lead',
+        'Location':'Quetta, Pakistan',
+        'Salary': 'Competetive',
+        
+    },
+    { 
+        'id':2,
+        'title':'Research Associate',
+        'Location':'Quetta, Pakistan',
+        'Salary': 'Competetive',
+    },
+{
+    'id':3,
+    'title':'Research Assistant',
+    'Location':'Quetta, Pakistan',
+    'Salary': 'Competitive',
+}
+]
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html',jobs=JOBS)
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
     
